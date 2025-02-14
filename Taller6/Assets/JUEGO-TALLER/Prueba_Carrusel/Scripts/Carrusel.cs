aa20b48bad7c10ab6ff30f1240a83896c53a4724
Garcial <miguelgarcialeiva@gmail.com>
2025-02-14 17:47:47 -0500

    Update carrusel
    arreglando el script que se me borró, aun no termino, pero para guardar los cambios

--- Taller6/Assets/JUEGO-TALLER/Prueba_Carrusel/Scripts/Carrusel.cs
+++ Taller6/Assets/JUEGO-TALLER/Prueba_Carrusel/Scripts/Carrusel.cs
@@ -1,16 +1,103 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Carrusel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    InputManager inputManager;
+
+    [SerializeField] RectTransform[] objetosCarrusel;
+    RectTransform objetoActual;
+
+    [SerializeField] float separacion;
+    [SerializeField] float tiempoSwipe;
+    [SerializeField] bool esVertical;
+
+    bool estaEnPrimero;
+    bool estaEnUltimo;
+
+    private void Awake()
     {
-        
+        inputManager = InputManager.Instanciar;
+
+        objetoActual = objetosCarrusel[0];
+
+        estaEnPrimero = true;
+        estaEnUltimo = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputManager.EnSwipeIzquierda += SwipeIzquierda;
+        inputManager.EnSwipeDerecha += SwipeDerecha;
+        inputManager.EnSwipeArriba += SwipeArriba;
+        inputManager.EnSwipeAbajo += SwipeAbajo;
+    }
+
+    private void OnDisable()
+    {
+        inputManager.EnSwipeIzquierda -= SwipeIzquierda;
+        inputManager.EnSwipeDerecha -= SwipeDerecha;
+        inputManager.EnSwipeArriba -= SwipeArriba;
+        inputManager.EnSwipeAbajo -= SwipeAbajo;
+    }
+
+    void SwipeIzquierda()
+    {
+        if (!esVertical || estaEnUltimo) return;
+        estaEnPrimero = false;
+
+        foreach (RectTransform objeto in objetosCarrusel)
+        {
+            objeto.DOAnchorPosX(objeto.localPosition.x - separacion, tiempoSwipe);
+
+            if (objeto.localPosition.y == 0) objetoActual = objeto;
+        }
+
+        if (objetoActual == objetosCarrusel[^1]) estaEnUltimo = true;
+    }
+
+    void SwipeDerecha()
+    {
+        if(!esVertical || estaEnPrimero) return;
+        estaEnUltimo = false;
+
+        foreach(RectTransform objeto in objetosCarrusel)
+        {
+            objeto.DOAnchorPosX(objeto.localPosition.x + separacion, tiempoSwipe);
+
+            if (objeto.localPosition.x == 0) objetoActual = objeto;
+        }
+
+        if (objetoActual == objetosCarrusel[0]) estaEnPrimero = true;
+    }
+
+    void SwipeArriba()
+    {
+        if (esVertical || estaEnUltimo) return;
+        estaEnPrimero = false;
+
+        foreach (RectTransform objeto in objetosCarrusel)
+        {
+            objeto.DOAnchorPosY(objeto.localPosition.y + separacion, tiempoSwipe);
+
+            if (objeto.localPosition.y == 0) objetoActual = objeto;
+        }
+
+        if (objetoActual == objetosCarrusel[^1]) estaEnUltimo = true;
+    }
+
+    void SwipeAbajo()
+    {
+        if (esVertical || estaEnPrimero) return;
+        estaEnUltimo = false;
+
+        foreach (RectTransform objeto in objetosCarrusel)
+        {
+            objeto.DOAnchorPosY(objeto.localPosition.y - separacion, tiempoSwipe);
+
+            if (objeto.localPosition.y == 0) objetoActual = objeto;
+        }
+
+        if (objetoActual == objetosCarrusel[^1]) estaEnPrimero = true;
     }
 }
